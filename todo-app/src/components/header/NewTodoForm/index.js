6448ca5fff31924fec1e1649f2a2aad5c75ef3a8
suheyl-yazici <h.suheylyazici@gmail.com>
2022-11-20 17:55:40 +0300

    todo uygulaması tamamlandı

---
+++ todo-app/src/components/header/NewTodoForm/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Formik, Field, Form } from "formik";
+import validationSchema from "./validations";
+import { useTodo } from "../../../contexts/TodoContext";
+
+const NewTodoForm = () => {
+  const { addTodo } = useTodo();
+
+  return (
+    <Formik
+      initialValues={{
+        text: "",
+      }}
+      onSubmit={(values, bag) => {
+        console.log(values);
+
+        addTodo(values.text)
+
+        bag.resetForm();
+      }}
+      validationSchema={validationSchema}
+    >
+      <Form>
+        <Field
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+          id="text"
+          name="text"
+        />
+      </Form>
+    </Formik>
+  );
+};
+
+export default NewTodoForm;
